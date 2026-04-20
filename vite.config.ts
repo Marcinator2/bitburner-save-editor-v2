dfbd6707e42f924d4600bf4dbb8802878f46bf54
Marcinator2 <marcinator_2@gmx.de>
2026-04-20 21:37:02 +0200

    changed pagelink

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import svgr from "vite-plugin-svgr";
 import tsconfigPaths from "vite-tsconfig-paths";
 
 export default defineConfig({
+  base: "/bitburner-save-editor-v2/",
   plugins: [
     react(),
     svgr(),
